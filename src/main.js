595dfb2a3eacb38aaa74f50f50dc0da262c783a5
one_piece <18330820519@163.com>
2019-04-11 22:57:40 +0800

    使用cdn引入依赖

--- src/main.js
+++ src/main.js
@@ -1,27 +1,28 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
+// import Vue from 'vue'
 import './assets/css/reset.css'
 import './assets/css/base.css'
-import 'element-ui/lib/theme-chalk/index.css';
+// import 'element-ui/lib/theme-chalk/index.css';
 // import 'lib-flexible/flexible'
-import ElementUI from 'element-ui';
-import VueFullPage from 'vue-fullpage.js'
+// import ElementUI from 'element-ui';
+// import VueFullPage from 'vue-fullpage.js'
 // import scroll from 'vue-seamless-scroll'
-import axios from 'axios'
-import Vueaxios from 'vue-axios'
-import Echarts from 'echarts'
+// import axios from 'axios'
+// import Vueaxios from 'vue-axios'
+// import Echarts from 'echarts'
 import App from './App'
-import router from './router'
+// import router from './router'
 
-Vue.config.productionTip = false
-Vue.prototype.echarts = Echarts
+// Vue.config.productionTip = false
+// Vue.prototype.echarts = Echarts
 // Vue.prototype.axios = axios
 
-Vue.use(ElementUI)
-Vue.use(VueFullPage)
-Vue.use(Echarts)
-Vue.use(Vueaxios,axios)
+// Vue.use(ElementUI)
+// Vue.use(VueFullPage)
+// Vue.use(Echarts)
+// Vue.use(Vueaxios,axios)
+// Vue.use(axios)
 // Vue.use(scroll)
 /* eslint-disable no-new */
 
